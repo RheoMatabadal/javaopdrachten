93247d5f330b09366e37317e480933e60e5a74db
rheo <rheo.matabadal.natin@gmail.com>
2017-06-24 20:52:59 -0300

    connection succesfull

--- periode 12/jdbctest/src/jdbctest/Driver.java
+++ periode 12/jdbctest/src/jdbctest/Driver.java
@@ -1,4 +1,19 @@
 package jdbctest;
+import java.sql.*;
 public class Driver {
+    private static final String USERNAME= "root";
+     private static final String PASSWORD= "root";
+      private static final String CONN_STRING= "jdbc:Mysql://127.0.0.1:3306/test?useSSL=false";
     
+    public static void main (String[]args){
+        Connection myConn = null;
+        try {
+           
+             myConn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("CONNECTED!");
+             }
+        catch (SQLException e){
+            System.err.println(e);
+        }
+    }
 }
